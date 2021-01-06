cd45126e5b5635a42f939c9529b3d3e4fa0a6056
OskarPrzydatek <o.przydatek@wp.pl>
2021-01-06 21:59:12 +0100

    smooth scroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BestsellersComponent } from './components/bestsellers/bestsellers.component';
 import { SearchBooksArticlesComponent } from './components/search-books-articles/search-books-articles.component';
-// import {BestsellersListComponent} from './components/bestsellers-list/bestsellers-list.component';
 
 const routes: Routes = [
-  // { path: '', component: BestsellersListComponent },
-  { path: '', component: BestsellersComponent },
-  { path: 'search', component: SearchBooksArticlesComponent}
+  {path: '', component: BestsellersComponent},
+  {path: 'search', component: SearchBooksArticlesComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
